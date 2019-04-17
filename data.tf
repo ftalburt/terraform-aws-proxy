d0e3404f51a7b888f0e12b2b19f92c981b7d8f0e
Forrest Talburt <2643911+ftalburt@users.noreply.github.com>
2019-04-17 17:04:18 -0500

    Always use IPv4 public IP

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "http" "ip" {
   # TODO: Replace below call with self-controlled URL
   # Assumes DEV computer has same IP as Terraform execution computer
-  url = "http://icanhazip.com"
+  url = "https://ipv4.icanhazip.com"
 }
 
 data "aws_availability_zones" "available" {}
